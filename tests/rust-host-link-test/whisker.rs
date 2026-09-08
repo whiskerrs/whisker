023bbd5b97b44a4702e99d03279ff008a5cc8924
Takeshi Tsukamoto <dev@itome.team>
2026-09-09 01:41:58 +0900

    Remove unused examples

--- tests/rust-host-link-test/whisker.rs
+++
@@ -1,6 +0,0 @@
-pub fn configure(app: &mut whisker_config::Config) {
-    app.name("Rust Host Link Test")
-        .bundle_id("rs.whisker.rusthostlinktest")
-        .version("0.0.0")
-        .build_number(1);
-}
